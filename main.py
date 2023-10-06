1e15755ab8fbeb5507d49a2dcdd5e31e42243d70
В. Н. А. СП МКН <st107903@student.spbu.ru>
2023-10-06 22:31:22 +0300

    create server

--- main.py
+++ main.py
@@ -1,11 +1,73 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fkbvkfjbjfbldsovfmvbkfmbfkbkjhkgkkldksdlklfdlfkkprkppcpkfkpewp'
+
+list_of_preferences = [
+    {"id": 0, "title": "Apple", "created": "later", "description": "NO"},
+    {"id": 1, "title": "Microsoft", "created": "now", "description": "YES"}
+]
 
 
-@app.route("/app")
-def members():
-    return {"members": ["1", "2", "3"]}
+def get_preference(preference_id):
+    try:
+        current_preference = list_of_preferences[preference_id]
+        return current_preference
+    except IndexError:
+        abort(404)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", posts=list_of_preferences)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    current_preference = get_preference(post_id)
+    return render_template('post.html', post=current_preference)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            list_of_preferences.append(
+                {"id": len(list_of_preferences), "title": title, "description": content, "created": "NOW"}
+            )
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    current_post = get_preference(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            list_of_preferences[id] = {
+                "id": id, "title": title, "created": "NOW", "description": content
+            }
+            return redirect(url_for('index'))
+    return render_template('edit.html', post=current_post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    current_preference = get_preference(id)
+    del list_of_preferences[id]
+    flash('"{}" was successfully deleted!'.format(current_preference['title']))
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
