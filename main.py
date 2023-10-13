fbb6a17435eee214e7ba17603f17f90081e03710
В. Н. А. СП МКН <st107903@student.spbu.ru>
2023-10-14 02:51:39 +0300

    structuring

--- main.py
+++ main.py
@@ -1,138 +1,22 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, g, jsonify
-from werkzeug.exceptions import abort
-from werkzeug.security import generate_password_hash
-from datetime import datetime, timedelta
+import flask
+from flask import render_template, request, url_for, flash, redirect, g, jsonify
 from flask_httpauth import HTTPBasicAuth
 
-from passlib.apps import custom_app_context as pwd_context
-from itsdangerous import (TimedJSONWebSignatureSerializer
-                          as Serializer, BadSignature, SignatureExpired)
+from user import *
 
 auth = HTTPBasicAuth()
-time = datetime.now()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fkbvkfjbjfbldsovfmvbkfmbfkbkjhkgkkldksdlklfdlfkkprkppcpkfkpewp'
 
-
-class User:
-    def __init__(self, name: str, id: int):
-        self.name = name
-        self.password_hash = None
-        self.id = id
-
-    def hash_password(self, password: str):
-        self.password_hash = pwd_context.encrypt(password)
-        return self
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
-
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    @staticmethod
-    def verify_auth_token(token):
-        serializer = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = serializer.loads(token)
-        except SignatureExpired:
-            return None  # valid token, but expired
-        except BadSignature:
-            return None  # invalid token
-        user = users[data['id']][0]
-        return user
-
-
-# check_password_hash(hash, "secret password")
-
-# some functions to work with DB
-
-list_of_preferences = [
-    {"id": 0, "title": "Apple", "created": time, "description": "She loves big red apples."},
-    {"id": 1, "title": "Kitten", "created": time, "description": "She loves small black kittens with black eyes."}
-]
-
-users = [
-    (User("USER_1", 0).hash_password("123"), list_of_preferences)
-]
-
-users_login_index = {
-    "USER_1": 0
-}
-
-
-class UsersClass:
-    @staticmethod
-    def get_user_by_name(name: str) -> User:
-        try:
-            return users[users_login_index[name]][0]
-        except KeyError:
-            return None
-
-    @staticmethod
-    def get_user_by_index(index_: int) -> User:
-        return users[index_][0]
-
-    @staticmethod
-    def get_user_tags(name: str) -> list:
-        try:
-            return users[users_login_index[name]][1]
-        except KeyError:
-            return None
-
-
-def get_current_preference(preference_id: int, user="USER_1"):
-    return users[users_login_index[user]][1][preference_id]
-
-
-def get_list_of_preferences(user="USER_1"):
-    return users[users_login_index[user]][1]
-
-
-def append_to_list_of_preferences(title, description, user="USER_1"):
-    users[users_login_index[user]][1].append(
-        {"id": len(list_of_preferences), "title": title, "description": description, "created": datetime.now()}
-        )
-
-
-def set_to_list_of_preferences(preference_id, title, description, user="USER_1"):
-    users[users_login_index[user]][0][preference_id] = {
-                "id": preference_id, "title": title, "created": datetime.now(), "description": description
-    }
-
-
-def delete_preference(preference_id, user="USER_1"):
-    del users[users_login_index[user]][0][preference_id]
-
-
-# replace all to get a correct behavior
-
-def get_preference(preference_id):
-    try:
-        current_preference = get_current_preference(preference_id)
-        return current_preference
-    except IndexError:
-        abort(404)
-
-
-# @auth.verify_password
-# def verify_password(username, password):
-#     user: User = users[username][0]
-#     if not user or not user.verify_password(password):
-#         return False
-#     g.user = user
-#     return True
+# later: change to normal secret_key
 
 
 @auth.verify_password
 def verify_password(username_or_token, password):
-    # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
     if not user:
-        # try to authenticate with username/password
-        user: User = UsersClass.get_user_by_name(username_or_token)
+        user: User = UsersGetter.get_user_by_name(username_or_token)
         if not user or not user.verify_password(password):
             return False
     g.user = user
@@ -141,7 +25,7 @@ def verify_password(username_or_token, password):
 
 @app.route("/")
 def index():
-    if g is None:
+    if g is None or g.user is None:
         return redirect(url_for('index'))
     return render_template("index.html", posts=get_list_of_preferences())
 
@@ -157,19 +41,8 @@ def get_auth_token():
 def login():
     if g.user is not None:
         return redirect(url_for('logout'))
-    # if request.method == 'POST':
-    #     username = request.json.get('username')
-    # password = request.json.get('password')
-    # if username is None or password is None:
-    #     abort(400) # missing arguments
-    # if User.query.filter_by(username = username).first() is not None:
-    #     abort(400) # existing user
-    # user = User(username = username)
-    # user.hash_password(password)
-    # db.session.add(user)
-    # db.session.commit()
-    # return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
     if request.method == 'POST':
+        # noinspection PyShadowingNames
         login = request.form['login']
         password = request.form['password']
         if not login:
@@ -177,21 +50,19 @@ def login():
         elif not password:
             flash('Password is required!')
         else:
-            user = User(name=login, id=len(users))
-            users_login_index[login] = len(users)
-            users.append((user, []))
-            user.hash_password(password)
-            # append_to_list_of_preferences(title, description)
+            verify_password(login, password)
         return redirect(url_for('index'))
     return render_template('login.html')
 
 
 @app.route("/logout", methods=('GET', 'POST'))
 @auth.login_required
-def logout():
+def logout():  # TODO incorrect log out
     if request.method == 'POST':
-        g.user = None
-        return url_for('login')
+        del g.user
+        flask.g = None
+        # verify_password("", "")
+        return redirect(url_for('index'))
     return render_template('logout.html')
 
 
@@ -200,6 +71,7 @@ def register():
     if g.user is not None:
         return redirect(url_for('logout'))
     if request.method == 'POST':
+        # noinspection PyShadowingNames
         login = request.form['login']
         password = request.form['password']
         if not login:
@@ -207,21 +79,19 @@ def register():
         elif not password:
             flash('Password is required!')
         else:
-            #append_to_list_of_preferences(title, description)
-            user = User(name=login, id=len(users))
-            users_login_index[login] = len(users)
-            users.append((user, []))
+            user = User(name=login, id=len(user_to_list_of_tag))
+            users_login_to_index[login] = len(user_to_list_of_tag)
+            user_to_list_of_tag.append((user, []))
             user.hash_password(password)
             return redirect(url_for('index'))
     return render_template('register.html')
 
 
-@app.route("/unregister", methods=('GET', 'POST'))
+@app.route("/unregister", methods=('GET', 'POST'))  # TODO incorrect result
 @auth.login_required
 def unregister():
     if request.method == 'POST':
-        # del users[users_login_index[g.user]] can't be deleted, another indexes will be invalid
-        del users_login_index[g.user]
+        del users_login_to_index[g.user]
         g.user = None
         return redirect(url_for('login'))
     return render_template('unregister.html')
@@ -230,7 +100,7 @@ def unregister():
 @app.route('/<int:post_id>')
 @auth.login_required
 def post(post_id):
-    current_preference = get_preference(post_id)
+    current_preference = get_tag(post_id, g.user.name)
     return render_template('post.html', post=current_preference)
 
 
@@ -243,31 +113,33 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            append_to_list_of_preferences(title, description)
+            append_to_list_of_preferences(title, description, g.user.name)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 
+# noinspection PyShadowingBuiltins
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 @auth.login_required
 def edit(id):
-    current_post = get_preference(id)
+    current_post = get_tag(id)
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['content']
         if not title:
             flash('Title is required!')
         else:
-            set_to_list_of_preferences(id, title, description)
+            set_to_list_of_preferences(id, title, description, g.user.name)
             return redirect(url_for('index'))
     return render_template('edit.html', post=current_post)
 
 
+# noinspection PyShadowingBuiltins
 @app.route('/<int:id>/delete', methods=('POST',))
 @auth.login_required
 def delete(id):
-    current_preference = get_preference(id)
+    current_preference = get_tag(id)
     delete_preference(id)
     flash('"{}" was successfully deleted!'.format(current_preference['title']))
     return redirect(url_for('index'))
---
+++ user.py
@@ -0,0 +1,120 @@
+from flask import Flask
+from werkzeug.exceptions import abort
+from datetime import datetime
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import \
+    (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+app: Flask
+time = datetime.now()
+
+
+class User:
+    # noinspection PyShadowingBuiltins
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.password_hash = None
+        self.id = id
+
+    def hash_password(self, password: str):
+        self.password_hash = pwd_context.encrypt(password)
+        return self
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        serializer = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = serializer.loads(token)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+        user = user_to_list_of_tag[data['id']][0]
+        return user
+
+# some functions to work with DB
+
+
+list_of_tag = [
+    {"id": 0, "title": "Apple", "created": time, "description": "She loves big red apples."},
+    {"id": 1, "title": "Kitten", "created": time, "description": "She loves small black kittens with black eyes."}
+]
+
+user_to_list_of_tag = [
+    (User("USER_1", 0).hash_password("123"), list_of_tag)
+]
+
+users_login_to_index = {
+    "USER_1": 0
+}
+
+
+class UsersGetter:
+    @staticmethod
+    def get_user_by_name(name: str) -> User:
+        try:
+            return user_to_list_of_tag[users_login_to_index[name]][0]
+        except KeyError:
+            # noinspection PyTypeChecker
+            return None  # correct behaviour for lib function
+
+    @staticmethod
+    def get_user_by_index(index_: int) -> User:
+        return user_to_list_of_tag[index_][0]
+
+    @staticmethod
+    def get_user_tags(name: str) -> list:
+        try:
+            return user_to_list_of_tag[users_login_to_index[name]][1]
+        except KeyError:  # correct behaviour for lib func
+            # noinspection PyTypeChecker
+            return None
+
+
+def get_current_preference(preference_id: int, user="USER_1"):
+    return UsersGetter.get_user_tags(user)[preference_id]
+
+
+def get_list_of_preferences(user="USER_1"):
+    return UsersGetter.get_user_tags(user)
+
+
+def append_to_list_of_preferences(title, description, user="USER_1"):
+    UsersGetter.get_user_tags(user).append(
+            {
+                "id": len(list_of_tag),
+                "title": title,
+                "description": description,
+                "created": datetime.now()
+            }
+        )
+
+
+def set_to_list_of_preferences(preference_id, title, description, user="USER_1"):
+    user_to_list_of_tag[users_login_to_index[user]][0][preference_id] = {
+                "id": preference_id,
+                "title": title,
+                "created": datetime.now(),
+                "description": description
+            }
+
+
+def delete_preference(preference_id, user="USER_1"):
+    del UsersGetter.get_user_tags(user)[preference_id]
+
+
+# replace all to get a correct behavior (when DB will be ready)
+
+def get_tag(tag_id, user="USER_1"):
+    try:
+        current_preference = get_current_preference(tag_id, user)
+        return current_preference
+    except IndexError:
+        abort(404)
