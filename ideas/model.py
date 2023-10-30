f0f3d82c89ba73ebfbf1a421a475482330010e1e
В. Н. А. СП МКН <st107903@student.spbu.ru>
2023-10-30 19:45:13 +0300

    feature(back-ii): finish with title-link

--- ideas/model.py
+++ ideas/model.py
@@ -1,15 +1,21 @@
+from ai import title_with_link
+
+
 class IdeaGenerator:
     def __init__(self):
         # noinspection PyTypeChecker
         self.__previous_ideas: list = None
 
-    def generate_ideas(self, tags: list, num_of_ideas: int, price_interval: int):
+    def generate_ideas(self, tags: list, num_of_ideas: int, price_interval: list):
+        link_title = title_with_link(str(tags)[1:-1], "друг", price_interval[1], num_of_ideas, price_interval[0])
+        if link_title is None:
+            self.__previous_ideas = None
         self.__previous_ideas = [
             {
-                "title": "House",
+                "title": link_title[0],
                 "description": "Good house!",
                 "img_link": "https://python.org",
-                "market_link": "https://python.org",
+                "market_link": link_title[1],
             }
         ]
         return self.__previous_ideas
--- t.py
+++ t.py
@@ -4,12 +4,13 @@ from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 
 
-url='https://megamarket.ru'
+url = 'https://megamarket.ru'
 response = requests.get(url)
 print(response.status_code)
 images = []
 
 soup = BeautifulSoup(response.text, features="html.parser")
+#print(soup) #
 img_tag = soup.find('img')
 print(img_tag)
 if img_tag:
