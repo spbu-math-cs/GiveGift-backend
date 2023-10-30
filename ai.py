9621033e3bd236866470fe96ca1faf8493bec82e
Tsagolov George Zaurbekovich <gtsagolov8@gmail.com>
2023-10-30 06:59:23 +0300

    intermediate result: wrote function that generates 5 present ideas with AI

---
+++ ai.py
@@ -0,0 +1,19 @@
+import openai
+from flask import Flask, request, jsonify
+
+openai.api_key = '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'
+
+
+def generate_present(context, role, max_budget):
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+            {"role": "system", "content": "Ты полезный ассистент, идеально подбирает подарки по предпочтения."},
+            {"role": "assistant", "content": f"Учти, {role} любит {context}."},
+            {"role": "user", "content": f"Учитывая эти предпочтения,"
+                f" выдай 5 идей подарка в двух-трех словах для {role} стоимостью до {max_budget} рублей,"
+                f" который можно купить в любом интернет-магазине"}
+        ])
+        recommendation = completion.choices[0].message.content
+        #print(recommendation)
+        return recommendation.split('\n')
+
+
