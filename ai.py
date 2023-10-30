addd7da07d64829afd5c0b31e639c124390bcb47
Tsagolov George Zaurbekovich <gtsagolov8@gmail.com>
2023-10-30 19:11:20 +0300

    intermediate result: wrote better request to ai, statred writing parsing of megamarket.ru

--- ai.py
+++ ai.py
@@ -1,16 +1,24 @@
 import openai
+import requests
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
-def generate_present(context, role, max_budget):
+def generate_present(context, role, max_budget, num_of_ideas):
+        if  num_of_ideas>5:
+            print("The number of ideas cannot exceed 5")
+            return -1
+        if num_of_ideas<1:
+            print("The number of ideas must be more than 1")
+            return -1
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
             {"role": "system", "content": "Ты полезный ассистент, идеально подбирает подарки по предпочтения."},
             {"role": "assistant", "content": f"Учти, {role} любит {context}."},
             {"role": "user", "content": f"Учитывая эти предпочтения,"
-                f" выдай 5 идей подарка в двух-трех словах для {role} стоимостью до {max_budget} рублей,"
-                f" который можно купить в любом интернет-магазине"}
+                f" выдай {num_of_ideas} идей подарка в двух-трех словах для {role} стоимостью до {max_budget} рублей,"
+                f" который можно купить в любом интернет-магазине. Выведи их , пронумеровав по порядку"}
         ])
         recommendation = completion.choices[0].message.content
         return recommendation.split('\n')
@@ -26,7 +34,13 @@ def link_to_market(str):
     return link
 
 
-def title_with_link(context, role, max_budget):
-    response = generate_present(context,role,max_budget)
+def get_img(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+def title_with_link(context, role, max_budget, num_of_ideas):
+    response = generate_present(context,role,max_budget,num_of_ideas)
     for link in response:
         print(link,": ", link_to_market(link),"\n")
+
+title_with_link("машины, игрушки", "друг",5000,3)
---
+++ t.py
@@ -0,0 +1,21 @@
+import openai
+import requests
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+
+
+url='https://megamarket.ru'
+response = requests.get(url)
+print(response.status_code)
+images = []
+
+soup = BeautifulSoup(response.text, features="html.parser")
+img_tag = soup.find('img')
+print(img_tag)
+if img_tag:
+        # Извлекаем ссылку на фотографию из атрибута src
+    photo_url = img_tag['src']
+    print("Ссылка на первую фотографию:", photo_url)
+else:
+    print("Фотографии не найдены на странице.")
+#print(images)
