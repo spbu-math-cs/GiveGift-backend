02c03b76e1ccaec335bec17b0073658f26b0510c
Tsagolov George Zaurbekovich <gtsagolov8@gmail.com>
2023-11-10 04:18:43 +0300

    wrote function, that gives us ID of product

--- parser.py
+++ parser.py
@@ -5,3 +5,8 @@ def queryLink(name,min_budg,max_budg):
         f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&curr=rub&dest=-1257786&priceU={min_budg}00;{max_budg}00&page=1&query={name}&resultset=catalog&sort=popular&spp=24&suppressSpellcheck=false',
     )
     return response.json()
+
+
+def getId(responce):
+    return responce['data']['products'][0]['id']
+
