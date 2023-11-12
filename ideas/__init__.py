343bd7c847b70ea779602aba540cc328bcc9ed19
AmEl <amirelkanov0504@gmail.com>
2023-11-12 22:16:15 +0300

    Refactoring

--- ideas/__init__.py
+++ ideas/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
-from .model import IdeaGenerator
 
 idea = Blueprint('idea', __name__)
-idea_generator = IdeaGenerator()
 
 from . import views
--- ideas/views.py
+++ ideas/views.py
@@ -1,34 +1,32 @@
 from flask import request
 
 from core import app
-from . import idea_generator
+from ideas.generator import generate_ideas
 
 
-@app.route('/generate_ideas', methods=["GET", "POST"])
+@app.route('/generate_ideas', methods=["POST"])
 def index():
-    if request.method == 'POST':
-        interests = request.json.get("interests", None)
-        num_of_ideas = request.json.get("num_of_ideas", None)
-        price_range = request.json.get("price_range", None)
-        if interests is None or num_of_ideas is None or price_range is None:
-            return {"response": "500", "message": "4011"}
-        if type(interests) is not list:
-            return {"response": "500", "message": "4012"}
-        try:
-            num_of_ideas = int(num_of_ideas)
-        except TypeError:
-            return {"response": "500", "message": "4013"}
-        if num_of_ideas not in range(1, 11):
-            return {"response": "500", "message": "4014"}
-        if type(price_range) is not list:  # price interval is list, contains two integers
-            return {"response": "500", "message": "4014"}
-        if len(price_range) != 2:
-            return {"response": "500", "message": "4015"}
-        try:
-            price_interval = [int(price_range[0]), int(price_range[1])]
-        except TypeError:
-            return {"response": "500", "message": "4016"}
-        return idea_generator.generate_ideas(interests, num_of_ideas, price_interval)
-    if idea_generator.has_previous_ideas():
-        return idea_generator.get_previous_ideas()
-    return {}
+    interests = request.json.get("interests", None)
+    num_of_ideas = request.json.get("num_of_ideas", None)
+    price_range = request.json.get("price_range", None)
+    # TODO: if interests is None или [] (надо будет посмотреть), возвращаем просто пустой массив, не давая gpt
+    if interests is None or num_of_ideas is None or price_range is None:
+        return {"response": "500", "message": "4011"}
+    if type(interests) is not list:
+        return {"response": "500", "message": "4012"}
+    try:
+        num_of_ideas = int(num_of_ideas)
+    except TypeError:
+        return {"response": "500", "message": "4013"}
+    if num_of_ideas not in range(1, 11):
+        return {"response": "500", "message": "4014"}
+    if type(price_range) is not list:  # price interval is list, contains two integers
+        return {"response": "500", "message": "4014"}
+    if len(price_range) != 2:
+        return {"response": "500", "message": "4015"}
+    try:
+        price_range = [int(price_range[0]), int(price_range[1])]
+    except TypeError:
+        return {"response": "500", "message": "4016"}
+
+    return generate_ideas(interests, num_of_ideas, price_range)
