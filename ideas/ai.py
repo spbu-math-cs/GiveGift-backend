4761a1af2a8ebcad1a658549d2a5e233088689da
verbinna22 <mylogininya@yandex.ru>
2023-11-12 18:09:38 +0300

    enchatement(back-ii): acceleration

--- ideas/ai.py
+++ ideas/ai.py
@@ -1,37 +1,53 @@
-from typing import Optional
 import asyncio
+
 import g4f
+from typing import List, Optional
 
 
-# noinspection PyBroadException
-async def ask_gpt_or_none_async(query: str) -> Optional[str]:
+def generate_link(title: str, min_budget: int, max_budget: int) -> Optional[str]:
+    return f"https://www.wildberries.ru/catalog/0/search.aspx?page=1&sort=popular&search={title}&priceU={min_budget}00%3B{max_budget}00"
+
+
+async def ask_gpt_or_none(num_of_ideas: int, preferences: List[str]):
+    if num_of_ideas <= 0:
+        return
     try:
         result: str = ""
-        response = (await asyncio.gather(
+        response: str = (await asyncio.gather(
             g4f.ChatCompletion.create_async(
                 model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": query}],
+                messages=[
+                    {"role": "system",
+                     "content": "Ты ассистент, который подбирает подарки по предпочтениям, без лишних разговоров и описаний."},
+                    {"role": "assistant", "content": f"Друг любит {', '.join(preferences)}."},
+                    {"role": "assistant", "content": f"Формат вывода: одним словом, через запятую'."},
+                    {"role": "user",
+                     "content": f"Выдай {num_of_ideas} уникальных идей подарка для друга без описания."},
+                ]
             )
         ))[0]
         for message in response:
             result += str(message)
-        return result
+        return result.replace('.', '').split(', ')
     except Exception as e:
         print(e)
         return None
 
 
-async def generate_title_or_none_async(tags: list, min_budget: int, max_budget: int, number_of_title: int = 0) -> \
-        Optional[str]:
-    result: Optional[str] = (await asyncio.gather(ask_gpt_or_none_async(
-        f"Сгенерируй одну идею, что подарить человеку, если он любит "
-        + str(tags)[
-          1: -1] + f", если я готов потратить от {min_budget} до {max_budget} рублей. Ответ выдай одним словом, которое содержит букву {'абвгдеёжзиклмнопрстуфхцчшщъыьэюя'[number_of_title % 33]}"
-    )))[0]
-    if len(result) > 1000:
-        return None
-    return result
+# Будем запускать гонкой: если один из запросов выдал быстрее чем остальные, остальные отменяются
+# Понимаю, что wait якобы устарел, но в gather'е нет такого флага, как FIRST_COMPLETED. Когда добавят, тогда и поговорим
+async def generate_ideas_or_none(tags: List[str], number_of_ideas: int, titles: List[str], lock: asyncio.Lock):
+    if number_of_ideas not in range(0, 10):
+        raise RuntimeError("Too much number_of_ideas!")
+    num_of_threads = 10
+    finished, unfinished = await asyncio.wait(
+        [ask_gpt_or_none(number_of_ideas, tags) for _ in range(num_of_threads)],
+        return_when="FIRST_COMPLETED"
+    )
+    await lock.acquire()
+    for idea in finished.pop().result():
+        titles.append(idea.capitalize())
+    lock.release()
 
 
-def generate_link(title: str, min_budget: int, max_budget: int) -> Optional[str]:
-    return f"https://www.wildberries.ru/catalog/0/search.aspx?page=1&sort=popular&search={title}&priceU={min_budget}00%3B{max_budget}00"
+# Если Exception in callback StreamRequest.release_curl() вылетать будет, не пугайся: это приколы в gpt4free, на это просто забить можно
--- ideas/model.py
+++ ideas/model.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from ideas.ai import generate_title_or_none_async, generate_link
+from ideas.ai import generate_ideas_or_none, generate_link
 from ideas.parser import get_image_link_or_none
 
 
@@ -9,41 +9,16 @@ class IdeaGenerator:
         # noinspection PyTypeChecker
         self.__previous_ideas: list = None
 
-    async def generate_one_idea(self, tags: list, price_interval: list, lock: asyncio.Lock, number_of_title: int = 0, deep: int = 0):
-        if deep > 100:
-            self.__previous_ideas = []
-            return
-        title: str = (await asyncio.gather(generate_title_or_none_async(tags=tags, min_budget=price_interval[0], max_budget=price_interval[1], number_of_title=number_of_title)))[0]
-        if title is None:
-            await asyncio.gather(self.generate_one_idea(tags, price_interval, lock, deep + 1))
-            return
-        link = generate_link(title=title, min_budget=price_interval[0], max_budget=price_interval[1])
-        image_link = get_image_link_or_none(product_name=title, min_budget=price_interval[0],
-                                            max_budget=price_interval[1])
-        if link is None or image_link is None:
-            await asyncio.gather(self.generate_one_idea(tags, price_interval, lock, deep + 1))
-            return
-        await lock.acquire()
-        self.__previous_ideas.append(
-            {
-                "title": title,
-                "description": "Обработка изображений поддерживается",
-                "img_link": image_link,
-                "market_link": link,
-            }
-        )
-        lock.release()
-
     def generate_ideas(self, tags: list, num_of_ideas: int, price_interval: list):
         self.__previous_ideas = []
-        if num_of_ideas > 5 or num_of_ideas <= 0:
-            raise RuntimeError("Unsupported")
+        titles = []
+        if num_of_ideas > 10 or num_of_ideas <= 0:
+            raise RuntimeError("Unsupported num_of_ideas not in 1..10")
         ioloop = asyncio.new_event_loop()
         asyncio.set_event_loop(ioloop)
         lock = asyncio.Lock()
         tasks = [
-            ioloop.create_task(self.generate_one_idea(tags=tags, price_interval=price_interval, lock=lock, number_of_title=i))
-            for i in range(num_of_ideas)
+            ioloop.create_task(generate_ideas_or_none(tags=tags, number_of_ideas=num_of_ideas, titles=titles, lock=lock))
         ]
         try:
             ioloop.run_until_complete(asyncio.wait(tasks))
@@ -51,6 +26,18 @@ class IdeaGenerator:
             pass
         finally:
             ioloop.close()
+        for title in titles:
+            link = generate_link(title=title, min_budget=price_interval[0], max_budget=price_interval[1])
+            image_link = get_image_link_or_none(product_name=title, min_budget=price_interval[0], max_budget=price_interval[1])
+            if link is not None or image_link is not None:
+                self.__previous_ideas.append(
+                    {
+                        "title": title,
+                        "description": "Обработка изображений поддерживается",
+                        "img_link": image_link,
+                        "market_link": link,
+                    }
+                )
         if self.__previous_ideas is None:
             self.__previous_ideas = []
         return self.__previous_ideas
