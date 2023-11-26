f511e24115d790dc1b3dc6afb90d882049be1591
DrozdovDan <daniil.drozdovjr@gmail.com>
2023-11-26 14:55:59 +0300

    added friendship system

--- database.py
+++ database.py
@@ -60,6 +60,11 @@ class User(UserMixin, Base):
     interests = relationship('Interest', secondary='user_interest', backref='User')
     password_hash = sa.Column(String(128))
 
+    # TODO: Check, why it doesn't work (+ fix m2m)
+    # friends = relationship('User', secondary='user_friend', backref='User')
+    # potential_friends = relationship('User', secondary='user_potential_friend', backref='User')
+    # friendship_applications = relationship('User', secondary='user_friendship_application', backref='User')
+
     def __repr__(self):
         return '<User {}>'.format(self.nickname)
 
@@ -82,21 +87,37 @@ class UserDatabase:
             sa.Column("user_id", sa.ForeignKey('User.id'), primary_key=True),
             sa.Column("interest_id", sa.ForeignKey('Interest.id'), primary_key=True)
         )
+        self.user_user_m2m_friend = self.db.Table(
+            "user_friend",
+            sa.Column("user_id", sa.ForeignKey('User.id'), primary_key=True),
+            sa.Column("friend_id", sa.ForeignKey('User.id'), primary_key=True)
+        )
+        self.user_user_m2m_potential_friend = self.db.Table(
+            "user_potential_friend",
+            sa.Column("user_id", sa.ForeignKey('User.id'), primary_key=True),
+            sa.Column("potential_friend_id", sa.ForeignKey('User.id'), primary_key=True)
+        )
+        self.user_user_m2m_friendship_application = self.db.Table(
+            "user_friendship_application",
+            sa.Column("user_id", sa.ForeignKey('User.id'), primary_key=True),
+            sa.Column("potential_friend_id", sa.ForeignKey('User.id'), primary_key=True)
+        )
 
     # TODO: check why foreign keys work so bad
 
     @_raises_database_exit_exception
-    def create_tables(self):
+    def create_tables(self) -> None:
         with self.app.app_context():
             self.db.create_all()
 
     @_raises_database_exit_exception
-    def drop_tables(self):
+    def drop_tables(self) -> None:
         with self.app.app_context():
             self.db.drop_all()
 
     @_raises_database_exit_exception
-    def add_user(self, id: int, nickname: str, email: str, birth_date: datetime.datetime, about: str, password: str):
+    def create_user(self, id: int, nickname: str, email: str, birth_date: datetime.datetime, about: str,
+                    password: str) -> None:
         with self.app.app_context():
             user = User()
             user.id = id
@@ -109,34 +130,34 @@ class UserDatabase:
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def add_interest(self, id: int, name: str):
+    def add_tag(self, id: int, name: str) -> None:
         with self.app.app_context():
             interest = Interest(id=id, name=name)
             self.db.session.add(interest)
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def add_user_interest(self, user_id: int, interest_id: int):
+    def add_user_tag(self, user_id: int, interest_id: int) -> None:
         with self.app.app_context():
             self.db.session.execute(sa.insert(self.user_interest_m2m).values(user_id=user_id, interest_id=interest_id))
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def delete_user_interest(self, user_id: int, interest_id: int):
+    def delete_user_tag(self, user_id: int, interest_id: int) -> None:
         with self.app.app_context():
             self.db.session.execute(sa.delete(self.user_interest_m2m).where(and_(
                 self.user_interest_m2m.c.user_id == user_id, self.user_interest_m2m.c.interest_id == interest_id)))
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def delete_user_by_id(self, user_id: int):
+    def delete_user(self, user_id: int) -> None:
         with self.app.app_context():
-            self.clear_user_interests(user_id)
+            self.clear_user_tags(user_id)
             self.db.session.query(User).filter_by(id=user_id).delete()
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def delete_interest_by_id(self, interest_id: int):
+    def delete_tag(self, interest_id: int) -> None:
         with self.app.app_context():
             self.db.session.execute(
                 sa.delete(self.user_interest_m2m).where(self.user_interest_m2m.c.interest_id == interest_id))
@@ -144,31 +165,31 @@ class UserDatabase:
             self.db.session.commit()
 
     @_raises_database_exit_exception
-    def get_user_by_id(self, user_id: int) -> User:
+    def get_user_by_index_or_none(self, user_id: int) -> User:
         with self.app.app_context():
             user = self.db.session.query(User).get(user_id)
             return user
 
     @_raises_database_exit_exception
-    def get_user_by_nickname(self, nickname: str) -> User:
+    def get_user_tags_by_name(self, nickname: str) -> User:
         with self.app.app_context():
             user = self.db.session.query(User).filter_by(nickname=nickname).first()
             return user
 
     @_raises_database_exit_exception
-    def get_user_by_email(self, email: str) -> User:
+    def get_user_by_email_or_none(self, email: str) -> User:
         with self.app.app_context():
             user = self.db.session.query(User).filter_by(email=email).first()
             return user
 
     @_raises_database_exit_exception
-    def get_interest_by_id(self, interest_id: int) -> Interest:
+    def get_tag_by_index_or_none(self, interest_id: int) -> Interest:
         with self.app.app_context():
             interest = self.db.session.query(Interest).filter_by(id=interest_id).first()
             return interest
 
     @_raises_database_exit_exception
-    def get_interest_by_name(self, name: str) -> Interest:
+    def get_tag_by_name_or_none(self, name: str) -> Interest:
         with self.app.app_context():
             interest = self.db.session.query(Interest).filter_by(name=name).first()
             return interest
@@ -179,22 +200,22 @@ class UserDatabase:
             return self.db.session.query(User).count()
 
     @_raises_database_exit_exception
-    def get_count_of_interests(self) -> int:
+    def get_count_of_tags(self) -> int:
         with self.app.app_context():
             return self.db.session.query(Interest).count()
 
     @_raises_database_exit_exception
-    def get_all_users(self) -> [User]:
+    def get_users(self) -> [User]:
         with self.app.app_context():
             return self.db.session.query(User).all()
 
     @_raises_database_exit_exception
-    def get_all_interests(self) -> [Interest]:
+    def get_tags(self) -> [Interest]:
         with self.app.app_context():
             return self.db.session.query(Interest).all()
 
     @_raises_database_exit_exception
-    def get_user_interests(self, user_id: int) -> [Interest]:
+    def get_user_tags(self, user_id: int) -> [Interest]:
         with self.app.app_context():
             select = self.db.session.execute(
                 sa.select(self.user_interest_m2m.c.interest_id).where(
@@ -205,7 +226,7 @@ class UserDatabase:
             return interests
 
     @_raises_database_exit_exception
-    def get_users_with_interest(self, interest_id: int) -> [User]:
+    def get_users_with_tag(self, interest_id: int) -> [User]:
         with self.app.app_context():
             select = self.db.session.execute(
                 sa.select(self.user_interest_m2m.c.user_id).where(
@@ -216,7 +237,7 @@ class UserDatabase:
             return users
 
     @_raises_database_exit_exception
-    def clear_user_interests(self, user_id: int):
+    def clear_user_tags(self, user_id: int) -> None:
         with self.app.app_context():
             self.db.session.execute(
                 sa.delete(self.user_interest_m2m).where(self.user_interest_m2m.c.user_id == user_id))
@@ -225,7 +246,7 @@ class UserDatabase:
     @_raises_database_exit_exception
     def has_user(self, email: str, password: str) -> bool:
         with self.app.app_context():
-            user = self.get_user_by_email(email)
+            user = self.get_user_by_email_or_none(email)
             if user is None:
                 return False
             return user.check_password(password)
@@ -237,7 +258,7 @@ class UserDatabase:
 
     @_raises_database_exit_exception
     def set_to_user_with_id(self, user_id: int, nickname: str, email: str, birth_date: datetime.datetime,
-                            about: str, password: str):
+                            about: str, password: str) -> None:
         with self.app.app_context():
             self.db.session.query(User).filter_by(id=user_id).update({
                 'nickname': nickname,
@@ -247,3 +268,107 @@ class UserDatabase:
                 'password_hash': generate_password_hash(password)
             }, synchronize_session=False)
             self.db.session.commit()
+
+    # friends part
+    @_raises_database_exit_exception
+    def add_friend(self, user_id: int, friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(sa.insert(self.user_user_m2m_friend).values(user_id=user_id, friend_id=friend_id))
+            self.db.session.execute(sa.insert(self.user_user_m2m_friend).values(user_id=friend_id, friend_id=user_id))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def remove_friend(self, user_id: int, friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(sa.delete(self.user_user_m2m_friend).where(and_(
+                self.user_user_m2m_friend.c.user_id == user_id, self.user_user_m2m_friend.c.friend_id == friend_id)))
+            self.db.session.execute(sa.delete(self.user_user_m2m_friend).where(and_(
+                self.user_user_m2m_friend.c.user_id == friend_id, self.user_user_m2m_friend.c.friend_id == user_id)))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def get_friends(self, user_id: int) -> [User]:
+        with self.app.app_context():
+            select = self.db.session.execute(
+                sa.select(self.user_user_m2m_friend.c.friend_id).where(
+                    self.user_user_m2m_friend.c.user_id == user_id)).fetchall()
+            friends = []
+            for friend_id in select:
+                friends.append(self.db.session.query(User).filter_by(id=friend_id[0]).first())
+            return friends
+
+    @_raises_database_exit_exception
+    def is_friend(self, user_id: int, friend_id: int) -> bool:
+        with self.app.app_context():
+            friends = self.get_friends(user_id)
+            return friend_id in list(map(lambda x: x.id, friends))
+
+    # potential friends part
+
+    @_raises_database_exit_exception
+    def add_potential_friend(self, user_id: int, potential_friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(
+                sa.insert(self.user_user_m2m_potential_friend).values(user_id=user_id,
+                                                                      potential_friend_id=potential_friend_id))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def remove_potential_friend(self, user_id: int, potential_friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(sa.delete(self.user_user_m2m_potential_friend).where(and_(
+                self.user_user_m2m_potential_friend.c.user_id == user_id,
+                self.user_user_m2m_potential_friend.c.potential_friend_id == potential_friend_id)))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def get_potential_friends(self, user_id: int) -> [User]:
+        with self.app.app_context():
+            select = self.db.session.execute(
+                sa.select(self.user_user_m2m_potential_friend.c.potential_friend_id).where(
+                    self.user_user_m2m_potential_friend.c.user_id == user_id)).fetchall()
+            friends = []
+            for friend_id in select:
+                friends.append(self.db.session.query(User).filter_by(id=friend_id[0]).first())
+            return friends
+
+    @_raises_database_exit_exception
+    def is_potential_friend(self, user_id: int, potential_friend_id: int) -> bool:
+        with self.app.app_context():
+            friends = self.get_potential_friends(user_id)
+            return potential_friend_id in list(map(lambda x: x.id, friends))
+
+    # friendship application part
+
+    @_raises_database_exit_exception
+    def add_application(self, user_id: int, potential_friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(
+                sa.insert(self.user_user_m2m_friendship_application).values(user_id=user_id,
+                                                                      potential_friend_id=potential_friend_id))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def remove_application(self, user_id: int, potential_friend_id: int) -> None:
+        with self.app.app_context():
+            self.db.session.execute(sa.delete(self.user_user_m2m_friendship_application).where(and_(
+                self.user_user_m2m_friendship_application.c.user_id == user_id,
+                self.user_user_m2m_friendship_application.c.potential_friend_id == potential_friend_id)))
+            self.db.session.commit()
+
+    @_raises_database_exit_exception
+    def get_applications(self, user_id: int) -> [User]:
+        with self.app.app_context():
+            select = self.db.session.execute(
+                sa.select(self.user_user_m2m_friendship_application.c.potential_friend_id).where(
+                    self.user_user_m2m_friendship_application.c.user_id == user_id)).fetchall()
+            friends = []
+            for friend_id in select:
+                friends.append(self.db.session.query(User).filter_by(id=friend_id[0]).first())
+            return friends
+
+    @_raises_database_exit_exception
+    def has_application(self, user_id: int, potential_friend_id: int) -> bool:
+        with self.app.app_context():
+            friends = self.get_applications(user_id)
+            return potential_friend_id in list(map(lambda x: x.id, friends))
