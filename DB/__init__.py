0a3fe3bd172458e208478634cb4d0d122f962ec7
verbinna22 <mylogininya@yandex.ru>
2023-12-14 21:31:51 +0300

    time switch

--- DB/__init__.py
+++ DB/__init__.py
@@ -2,7 +2,7 @@ from .DBMain import UserDatabase
 
 from .DBImitation import DataDecorator
 
-# data_base: DataDecorator = DataDecorator()
+data_base: DataDecorator = DataDecorator()
 
-data_base: UserDatabase = UserDatabase(database='sqlite:///user.db')
-data_base.create_tables()
+# data_base: UserDatabase = UserDatabase(database='sqlite:///user.db')
+# data_base.create_tables()
