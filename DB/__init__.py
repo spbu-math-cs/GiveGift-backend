1eb4c8108a14160812bd6a60d4d267488942db5a
AmEl <amirelkanov0504@gmail.com>
2023-12-20 16:19:40 +0300

    deleted dbimitation

--- DB/__init__.py
+++ DB/__init__.py
@@ -1,8 +1,4 @@
 from .DBMain import UserDatabase
 
-from .DBImitation import DataDecorator
-
-# data_base: DataDecorator = DataDecorator()
-
 data_base: UserDatabase = UserDatabase(database='sqlite:///user.db')
 data_base.create_tables()
