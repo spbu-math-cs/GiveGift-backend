c8983b49aba809937dc947e02db26ce15e190e1d
verbinna22 <mylogininya@yandex.ru>
2023-12-26 14:53:06 +0300

    fix(back-tests): delete odd tests

--- tests/test.py
+++ tests/test.py
@@ -29,11 +29,6 @@ def runner(my_app):
     return my_app.test_cli_runner()
 
 
-def test_request_example(client):
-    # response = client.get("/posts")
-    assert True  # b"<h2>Hello, World!</h2>" in response.data
-
-
 def test_register_user(client):
     response = client.post("/register", json={
         "nickname": "Flask",
